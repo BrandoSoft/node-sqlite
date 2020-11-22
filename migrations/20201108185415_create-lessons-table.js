583b1c38a19ebbad153a102ed8f97e17f09a8796
BrandoSoft <csbrandox@gmail.com>
2020-11-22 18:35:41 +0100

    added pool

--- migrations/20201108185415_create-lessons-table.js
+++ migrations/20201108185415_create-lessons-table.js
@@ -18,6 +18,7 @@ exports.up = function (knex) {
 
             tbl.integer('lessonId')
                 .unsigned()
+                .notNullable()
                 .references('id')
                 .inTable('lessons')
                 .onDelete('CASCADE')
